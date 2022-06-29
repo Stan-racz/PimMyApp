cf4841f549582441e41e1d9b5d12d2f6ce5807b6
lpinto <pinto.laetitia@lessonsharing.fr>
2022-06-29 16:36:57 +0200

    PYMAF-0: ajout des routes

--- pimmyfront/src/app/shared/sidebar/menu-items.ts
+++ pimmyfront/src/app/shared/sidebar/menu-items.ts
@@ -4,7 +4,7 @@ export const ROUTES: RouteInfo[] = [
  
   {
     path: '/dashboard',
-    title: 'Dashboard',
+    title: 'Accueil',
     icon: 'bi bi-speedometer2',
     class: '',
     extralink: false,
@@ -81,5 +81,110 @@ export const ROUTES: RouteInfo[] = [
     class: '',
     extralink: false,
     submenu: []
-  }
+  },
+  {
+    path: '/component/demande-conges',
+    title: 'Demande de congés',
+    icon: 'bi bi-calendar-plus',
+    class: '',
+    extralink: false,
+    submenu: []
+  },
+  {
+    path: '/component/visualisation',
+    title: 'Visualisation historique',
+    icon: 'bi bi-calendar3',
+    class: '',
+    extralink: false,
+    submenu: []
+  },
+  {
+    path: '/component/convention-collective',
+    title: 'Convention collective',
+    icon: 'bi bi-clipboard',
+    class: '',
+    extralink: false,
+    submenu: []
+  },
+  {
+    path: '/component/validation-conges-manager',
+    title: 'Validation congés',
+    icon: 'bi bi-check2-circle',
+    class: '',
+    extralink: false,
+    submenu: []
+  },
+  {
+    path: '/component/visualisation-absences',
+    title: 'Visualisation absences',
+    icon: 'bi bi-eye',
+    class: '',
+    extralink: false,
+    submenu: []
+  },
+  {
+    path: '/component/ajout-utilisateur',
+    title: 'Ajout utilisateur',
+    icon: 'bi bi-person-plus',
+    class: '',
+    extralink: false,
+    submenu: []
+  },
+  {
+    path: '/component/services',
+    title: 'Services',
+    icon: 'bi bi-gear',
+    class: '',
+    extralink: false,
+    submenu: []
+  },
+  {
+    path: '/component/synthese-conges',
+    title: 'Synthèse des congés',
+    icon: 'bi bi-clipboard-data',
+    class: '',
+    extralink: false,
+    submenu: []
+  },
+  {
+    path: '/component/validation-conges',
+    title: 'Validation congés',
+    icon: 'bi bi-check2-circle',
+    class: '',
+    extralink: false,
+    submenu: []
+  },
+  {
+    path: '/component/fiche-utilisateur',
+    title: 'Fiche utilisateur',
+    icon: 'bi bi-person',
+    class: '',
+    extralink: false,
+    submenu: []
+  },
+  {
+    path: '/component/motif-absence',
+    title: 'Motif d\'absence',
+    icon: 'bi bi-calendar2-x',
+    class: '',
+    extralink: false,
+    submenu: []
+  },
+  {
+    path: '/component/fermeture-institution',
+    title: 'Fermeture institution',
+    icon: 'bi bi-x-octagon',
+    class: '',
+    extralink: false,
+    submenu: []
+  },
+  {
+    path: '/component/visibilite-employes',
+    title: 'Visibilité des employés',
+    icon: 'bi bi-eye',
+    class: '',
+    extralink: false,
+    submenu: []
+  },
+  
 ];
