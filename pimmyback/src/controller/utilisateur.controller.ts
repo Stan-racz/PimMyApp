65fb35070f3da64b4f9f6163761f4a647303803c
AmZerdine <amandine.fusillier@outlook.fr>
2022-07-06 15:45:43 +0200

    PYMAB-1.0 : ajout util CSV

--- pimmyback/src/controller/utilisateur.controller.ts
+++ pimmyback/src/controller/utilisateur.controller.ts
@@ -1,8 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { UtilisateurService } from '../service/utilisateur.service';
 
 //on peut donner une route a un controller (ici localhost:3000/hello/world va retourner getHello. Aucune autre route ne la retournera)
 @Controller()
 export class UtilController {
   constructor(private readonly utilService: UtilisateurService) { }
+  @Post()
+  createPost(@Body() body: string) {
+    return `Created a new post with values of ${JSON.stringify(body)} 🚀`;
+  }
 }
