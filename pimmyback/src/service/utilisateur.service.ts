835ba5ea234027926a4ccbd51d0f58ea7f493fe2
AmZerdine <amandine.fusillier@outlook.fr>
2022-07-06 16:09:27 +0200

    PYMAB-1.0 : ajout util MANUEL

--- pimmyback/src/service/utilisateur.service.ts
+++ pimmyback/src/service/utilisateur.service.ts
@@ -17,7 +17,14 @@ export class UtilisateurService {
 //   findOne(id: number): Promise<Utilisateur> {
 //     return this.utilRepo.findOne(id);
 //   }
-
+  async create(nom: string, prenom: string, email: string, civilite: string, status: string, dateNaiss: string, nbHeureContractuelle: number): Promise<void> {
+    await this.utilRepo
+    .createQueryBuilder()
+    .insert()
+    .into(Utilisateur)
+    .values([{ nom: "nom", prenom: "prenom", email: "email@mail.com",civilite: "civilite", status: "status", dateNaiss: "00/00/0000",nbHeureContractuelle:1600}])
+    .execute();
+  }
   async remove(id: number): Promise<void> {
     await this.utilRepo.delete(id);
   }
