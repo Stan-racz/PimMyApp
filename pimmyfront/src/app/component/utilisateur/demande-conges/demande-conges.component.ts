90ba01338941126a190470344e3e8e21374c974d
lpinto <pinto.laetitia@lessonsharing.fr>
2022-09-29 16:41:30 +0200

    Ajout formulaire pour ajouter un congé

--- pimmyfront/src/app/component/utilisateur/demande-conges/demande-conges.component.ts
+++ pimmyfront/src/app/component/utilisateur/demande-conges/demande-conges.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
-import { CalendarOptions } from '@fullcalendar/angular';
+import { CalendarOptions, DateSelectArg } from '@fullcalendar/angular';
+import listPlugin from '@fullcalendar/list';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-demande-conges',
@@ -8,29 +10,80 @@ import { CalendarOptions } from '@fullcalendar/angular';
   styleUrls: ['./demande-conges.component.scss']
 })
 export class DemandeCongesComponent implements OnInit {
-  events: any = [
-    {title: "Present", date: '2022-09-01', color: "#0000FF"},
-    {title: "Absent", date: '2022-09-09', color: "#FF0000"},
-    {title: "Absent", date: '2022-09-11', color: "#FF0000"},
-  ] 
+  
+  motifs = ['Congés payés', 'Congés sans solde', 'RTT'];
+  debut: any;
+  fin: any;
+  displayStyle = "none";
+  calendar: any;
 
   calendarOptions: CalendarOptions = {
+    plugins: [listPlugin],
+    headerToolbar: {
+      left: 'dayGridMonth,listWeek',
+      center: 'title',
+      right: 'prev next today',
+
+    },
+    buttonText: {
+      today: 'Aujourd\'hui',
+      list: 'Liste',
+      month: "Mois",
+    },
     initialView: 'dayGridMonth',
-    weekends: true ,
-    events: this.events,
+    weekends: true,
+    editable: true,
+    selectable: true,
+    events: [
+      { title: "Present", date: '2022-09-01', color: "#0000FF" },
+      { title: "Absent", date: '2022-09-09', color: "#FF0000" },
+      { title: "Absent", date: '2022-09-11', color: "#FF0000" },
+    ],
     locale: 'fr',
     firstDay: 1,
-    selectable: true
-    
+    select: this.openPopup.bind(this),
   };
 
   toggleWeekends() {
-    this.calendarOptions.weekends = !this.calendarOptions.weekends 
+    this.calendarOptions.weekends = !this.calendarOptions.weekends
   }
-  
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  openPopup(selectInfo: DateSelectArg) {
+    this.displayStyle = "block";
+    this.debut = selectInfo.startStr;
+    this.fin = selectInfo.endStr;
+    this.calendar = selectInfo.view.calendar;
+
+    this.calendar.unselect();
+  }
+
+  closePopup() {
+    this.displayStyle = "none";
+  }
+
+  onSubmit(form: NgForm) {
+
+    const title = form.value.motif;
+
+    if (form.submitted) {
+      this.calendar.addEvent({
+        title,
+        start: form.value.debut,
+        end: form.value.fin,
+        allDay: true,
+        color: "#FF0000",
+      });
+      
+      form.resetForm();
+      this.closePopup();
+
+    }
+
   }
 
 }
